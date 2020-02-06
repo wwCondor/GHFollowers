6805524ec53b21ce7f793af7cbc098eb78fb3880
wwCondor <wouter@wearesure.io>
2020-02-06 19:04:01 +0100

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Wouter Willebrands on 06/02/2020.
+//  Copyright © 2020 CodingCondor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
