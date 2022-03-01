87a5339b28b76ff8e98a09aaf0237416ec1568b8
Wouter Willebrands <wouter.willebrands@9to5.software>
2022-03-01 19:28:54 +0100

    Create Tag.swift

---
+++ GHFollowers/Model/Tag.swift
@@ -0,0 +1,15 @@
+//
+//  Tag.swift
+//  GHFollowers
+//
+//  Created by Wouter Willebrands on 01/03/2022.
+//  Copyright © 2022 CodingCondor. All rights reserved.
+//
+
+import Foundation
+
+/// View tags used for dismissing specific views.
+enum Tag: Int {
+    case activityIndicator = 1
+    case emptyStateView = 0
+}
