bfc408ab5eda5e1be87e525b833af63b4e663c2e
Wouter Willebrands <wouter.willebrands@9to5.software>
2022-03-01 19:25:49 +0100

    Replace downloadImage method with UIImage extension

--- GHFollowers/CustomView/ImageView/GFAvatarImageView.swift
+++ GHFollowers/CustomView/ImageView/GFAvatarImageView.swift
@@ -25,11 +25,4 @@ class GFAvatarImageView: UIImageView {
         translatesAutoresizingMaskIntoConstraints = false
         image = GFImages.avatarPlaceholder
     }
-    
-    func downloadImage(fromURL url: String) {
-        NetworkManager.shared.downloadImage(from: url) { [weak self] image in
-            guard let self = self else { return }
-            DispatchQueue.main.async { self.image = image } 
-        }
-    }
 }
